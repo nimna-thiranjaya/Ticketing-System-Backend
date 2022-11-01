0783be2a5254abc5ec2be5a614205839bd830388
Nimna Thiranjaya <nimnathiranjya523@gmail.com>
2022-11-02 00:06:04 +0530

    ADD : Get on , Get off functions

--- controllers/driver.controller.js
+++ controllers/driver.controller.js
@@ -1,5 +1,9 @@
 const BusStateTemp = require("../models/busStateTemp.model");
 const Bus = require("../models/bus.model");
+const UserTravel = require("../models/userTravel.model");
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const allRoutes = require("../data/route.data");
 
 const ChangeBusState = async (req, res) => {
   const { busState } = req.body;
@@ -8,7 +12,7 @@ const ChangeBusState = async (req, res) => {
 
   const busDetails = await Bus.findOne({ _id: logedUser.busDetails._id });
 
-  console.log(busDetails);
+  //console.log(busDetails);
   if (busState == "Running") {
     const busStatetemcheck = await BusStateTemp.findOne({
       driverID: logedUser._id,
@@ -30,7 +34,9 @@ const ChangeBusState = async (req, res) => {
       } else {
         await Bus.findByIdAndUpdate(logedUser.busDetails._id, {
           busState: busState,
+          BusRunningID: newStateChaged._id,
         });
+
         return res
           .status(200)
           .send({ status: true, message: "Bus State Changed" });
@@ -51,6 +57,7 @@ const ChangeBusState = async (req, res) => {
     } else {
       await Bus.findByIdAndUpdate(logedUser.busDetails._id, {
         busState: busState,
+        BusRunningID: null,
       });
       return res
         .status(200)
@@ -64,6 +71,7 @@ const ChangeBusState = async (req, res) => {
       await BusStateTemp.findOneAndDelete({ busId: logedUser.busDetails._id });
       await Bus.findByIdAndUpdate(logedUser.busDetails._id, {
         busState: busState,
+        BusRunningID: null,
       });
       return res
         .status(200)
@@ -71,6 +79,7 @@ const ChangeBusState = async (req, res) => {
     } else {
       await Bus.findByIdAndUpdate(logedUser.busDetails._id, {
         busState: busState,
+        BusRunningID: null,
       });
       return res
         .status(200)
@@ -83,4 +92,119 @@ const ChangeBusState = async (req, res) => {
   }
 };
 
-module.exports = { ChangeBusState };
+const calculateFare = (totalLength) => {
+  if (totalLength <= 5) {
+    return 30;
+  } else {
+    return 30 + (totalLength - 5) * 3;
+  }
+};
+
+const PassengerGetOn = async (req, res) => {
+  try {
+    const logedUser = req.logedUser;
+    const { QRToken, getOnHoltID, getOffHoltID } = req.body;
+
+    var UserData = jwt.verify(QRToken, process.env.JWT_SECRET_QR);
+    var passenger = await User.findOne({ _id: UserData.userID });
+    var totalLength = Math.abs((getOnHoltID - getOffHoltID) * 4);
+    var bus = await Bus.findOne({ driverID: logedUser._id });
+    var fare = calculateFare(totalLength);
+    var runningBusTemp = await BusStateTemp.findOne({
+      _id: bus.BusRunningID,
+    });
+
+    if (!runningBusTemp.remainingSeats == 0) {
+      console.log(passenger.totalCredit);
+      if (passenger.totalCredit >= fare) {
+        const route = allRoutes.find(
+          (route) => route.routeId == logedUser.route
+        );
+        const getOnHoltName = route.holts.find(
+          (holt) => holt.holtId == getOnHoltID
+        ).holtName;
+        const getOffHoltName = route.holts.find(
+          (holt) => holt.holtId == getOffHoltID
+        ).holtName;
+
+        const data = {
+          passengerID: passenger._id,
+          busID: bus._id,
+          busRunningID: bus.BusRunningID,
+          busNumber: bus.busNumber,
+          routeNo: logedUser.route,
+          routeName: route.routeName,
+          getOnHoltID: getOnHoltID,
+          getOnHoltName: getOnHoltName,
+          getOffHoltID: getOffHoltID,
+          getOffHoltName: getOffHoltName,
+          getOnTime: new Date(),
+          getOffTime: null,
+          ticketPrice: fare,
+        };
+
+        const newPassengerTravel = await UserTravel.create(data);
+        if (newPassengerTravel) {
+          await User.findByIdAndUpdate(passenger._id, {
+            totalCredit: passenger.totalCredit - fare,
+          });
+
+          await Bus.findByIdAndUpdate(bus._id, {
+            totalEarning: bus.totalEarning + fare,
+          });
+
+          await BusStateTemp.findByIdAndUpdate(bus.BusRunningID, {
+            remainingSeats: runningBusTemp.remainingSeats - 1,
+          });
+
+          return res.status(200).send({
+            status: true,
+            message: "Passenger Travel Created",
+          });
+        } else {
+          return res.status(500).send({
+            status: false,
+            message: "Something went wrong",
+          });
+        }
+      } else {
+        return res
+          .status(400)
+          .send({ status: false, message: "Insufficient Credit" });
+      }
+    } else {
+      return res
+        .status(400)
+        .send({ status: false, message: "No Seats Available" });
+    }
+
+    // console.log(fare);
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
+
+const PassengerGetOff = async (req, res) => {
+  try {
+    const { QRToken } = req.body;
+    const logedUser = req.logedUser;
+    var UserData = jwt.verify(QRToken, process.env.JWT_SECRET_QR);
+    var bus = await Bus.findOne({ driverID: logedUser._id });
+    var busRunning = await BusStateTemp.findOne({ _id: bus.BusRunningID });
+
+    await UserTravel.findOneAndUpdate(
+      { busRunningID: bus.BusRunningID, passengerID: UserData.userID },
+      { getOffTime: new Date() }
+    );
+
+    await BusStateTemp.findByIdAndUpdate(bus.BusRunningID, {
+      remainingSeats: busRunning.remainingSeats + 1,
+    });
+
+    return res.status(200).send({ status: true, message: "Passenger Get Off" });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
+
+module.exports = { ChangeBusState, PassengerGetOn, PassengerGetOff };
--- routes/driver.routes.js
+++ routes/driver.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const DriverRouter = express.Router();
 
-const { ChangeBusState } = require("../controllers/driver.controller");
+const {
+  ChangeBusState,
+  PassengerGetOn,
+  PassengerGetOff,
+} = require("../controllers/driver.controller");
 const userAuth = require("../middlewares/user.middleware");
 
 DriverRouter.post("/changeBusState", userAuth, ChangeBusState);
+DriverRouter.post("/passengerGetOn", userAuth, PassengerGetOn);
+DriverRouter.post("/passengerGetOff", userAuth, PassengerGetOff);
 
 module.exports = DriverRouter;
